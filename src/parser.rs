fe146258446ff071f337d17f7be6147b84891af0
colevanh <colevanh@gmail.com>
2026-07-15 11:38:55 -0700

    Introductory tests for nth_word function

--- src/parser.rs
+++ src/parser.rs
@@ -14,17 +14,38 @@ pub fn nth_word(my_str: &str, x: u32) -> &str {
         
 
             if counter == x - 1 {
-                println!("Reached:");
                 starting_index = i + 1;
             }
 
             if counter == x {
                 ending_index = i;
-                println!("starting: {}", starting_index);
-                println!("ending: {}", ending_index);
                 return &my_str[starting_index..ending_index]
             }
         }
     }
-    &my_str[..]
+
+    &my_str[starting_index..]
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_STRING_1: &str = "Hey Tamra Get Lost";
+    const TEST_STRING_2: &str = "hello";
+    const TEST_STRING_3: &str = "a f d f sd 3dd fd d3 f sdd s";
+    const TEST_STRING_EMPTY: &str = "";
+
+    #[test]
+    fn test_nth_word() {
+        assert_eq!(nth_word(TEST_STRING_1, 2), "Tamra");
+        assert_eq!(nth_word(TEST_STRING_2, 1), "hello");
+        assert_eq!(nth_word(TEST_STRING_3, 6), "3dd");
+        assert_eq!(nth_word(TEST_STRING_3, 30), "a f d f sd 3dd fd d3 f sdd s");
+        assert_eq!(nth_word(TEST_STRING_EMPTY, 1), "");
+
+    }
+
+}
+
+
